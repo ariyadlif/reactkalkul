6e0453eb5f421fa822d5d4706f4e6e32022e4100
User achmadriyadlifachmi <ar.arf.rf@gmail.com>
2019-11-30 07:40:35 +0700

    kalkulator sederhana react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+  
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submit: false,
+      hasil:0,
+      angka1:0,
+      angka2:0,
+      operator:""
+    };
+  }
+
+  changeHandler = (event) => {
+    let value = event.target.value
+    this.setState({[event.target.name]:value})
+  }
+
+  submitHandler = () => {
+    let angka1 = parseInt(this.state.angka1)
+    let angka2 = parseInt(this.state.angka2)
+    let operator = this.state.operator
+    let hasil= 0
+    if(operator==="+"){
+      hasil=angka1 + angka2
+    }else if(operator==="-"){
+      hasil=angka1 - angka2
+    }else if(operator==="*"){
+      hasil=angka1 * angka2
+    }
+    this.setState({
+      hasil,
+      submit:!this.state.submit
+    })
+  }
+
+  backHandler = () => {
+    this.setState({
+      submit:!this.state.submit
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <center>
+          {!this.state.submit ? (
+            <div>
+              <input type="number" name="angka1" placeholder="Angka 1" onChange={this.changeHandler}/>
+              <br/><br/>
+
+              <select name="operator" onChange={this.changeHandler}>
+                <option value="">=Pilih Operator=</option>
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+              </select>
+              <br/><br/>
+
+              <input type="number" name="angka2" placeholder="Angka 2"  onChange={this.changeHandler}/>
+              <br/><br/>
+              
+              <input type="submit" onClick={() => this.submitHandler()} value="Hasil"/>
+            </div>
+          ):(
+            <div>
+              <h1>Hasil {this.state.angka1} {this.state.operator} {this.state.angka2} adalah {this.state.hasil} </h1> 
+              <button onClick={() => this.backHandler()}>Back To Home</button>
+            </div>
+          )}
+        </center>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
